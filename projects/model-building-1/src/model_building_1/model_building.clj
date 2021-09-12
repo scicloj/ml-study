a4ff10d92dc74eaf6a2ce29794a703b495ca58b9
daslu <daniel.slutsky@gmail.com>
2021-09-13 00:01:44 +0300

    initial explorations of the Model building section of R4DS

---
+++ projects/model-building-1/src/model_building_1/model_building.clj
@@ -0,0 +1,98 @@
+(ns model-building-1.model-building
+  (:require [notespace.api]))
+
+(require '[tablecloth.api :as table]
+         '[tech.v3.dataset.modelling :as modelling]
+         '[tech.v3.dataset :as dataset]
+         '[scicloj.metamorph.core :as morph]
+         '[scicloj.ml.core :as ml]
+         '[scicloj.metamorph.ml :as morphml]
+         '[scicloj.metamorph.ml.loss :as loss]
+         '[scicloj.ml.metamorph :as mlmorph]
+         '[scicloj.ml.smile.classification]
+         '[fastmath.clustering :as clustering]
+         '[fastmath.random :as random]
+         '[scicloj.viz.api :as viz]
+         '[aerial.hanami.common :as hc]
+         '[aerial.hanami.templates :as ht]
+         '[notespace.kinds :as kind]
+         '[tech.viz.vega])
+
+;; Following R4DS section 24: Model building
+
+(defonce diamonds ; from the R package ggplot2
+  (table/dataset "data/diamonds.csv"
+                 {:key-fn keyword}))
+
+(defonce flights ; from the R package nycflights13
+  (table/dataset "data/flights.csv"
+                 {:key-fn keyword}))
+
+;; ggplot(diamonds, aes(cut, price)) + geom_boxplot()
+
+^kind/hiccup
+(into [:div]
+      (for [y-column [;; "cut" "color"
+                      "clarity"]]
+        [:div
+         [:p
+          [:big y-column]]
+         [:p/vega
+          (-> diamonds
+              (table/select-columns [(keyword y-column) :price])
+              (viz/data)
+              (viz/y y-column {:type "nominal"})
+              (viz/x "price")
+              (viz/type "boxplot")
+              (assoc :SIZE 30)
+              viz/viz)]]))
+
+
+^kind/hiccup
+(into [:div]
+      (for [clarity-value ["I1" "IF"]]
+        [:div
+         [:p [:big clarity-value]]
+         [:p/vega
+          (-> diamonds
+              (table/select-columns [:clarity :price])
+              (table/select-rows (fn [row]
+                                   (-> row
+                                       :clarity
+                                       (= clarity-value))))
+              :price
+              (tech.viz.vega/histogram "price"))]]))
+
+
+
+
+(-> diamonds
+    (viz/data)
+    (viz/x "carat")
+    (viz/y "price")
+    (viz/type "point")
+    viz/viz)
+
+
+
+(def rect-chart
+  (assoc ht/view-base
+         :mark (merge ht/mark-base {:type "rect"})))
+
+
+(-> diamonds
+    (table/select-columns [:carat :price])
+    (viz/data)
+    (viz/type rect-chart)
+    (viz/x "carat" {:XBIN {:maxbins 10}})
+    (viz/y "price" {:YBIN {:maxbins 10}})
+    (merge {:COLOR {:aggregate "count"
+                    :type "quantitative"}})
+    viz/viz
+    (update-in [:encoding :tooltip]
+               conj {:aggregate "count" :type "quantitative"}))
+
+
+
+
+:ok
